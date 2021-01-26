8d9478b1f3e805306ee168888c90f9c1b9839c27
Anthony Sim <anthonyysim@gmail.com>
2021-01-26 08:31:44 -0800

    added express json and urlencoded middleware

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 // STATIC FILE
-app.use(express.static('public'))
+app.use(express.static('client'))
+
+// recognize the incoming Request Object as a JSON Object.
+app.use(express.json())
+// app.use(bodyParser.json());
+
+// method inbuilt in express to recognize the incoming Request Object as strings or arrays.
+app.use(express.urlencoded({ extended: true }))
+
+
+app.post('/upload_json', (req, res) => {
+  let data = req.body
+  console.log(data)
+  res.redirect('/');
+})
+
 
 // SERVER CONNECTION
 app.listen(port, () => {
