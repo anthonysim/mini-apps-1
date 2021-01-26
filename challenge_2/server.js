b7ad40f20cf454695f0b05a3efdf072226654342
Anthony Sim <anthonyysim@gmail.com>
2021-01-25 20:21:55 -0800

    added static file

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+// STATIC FILE
+app.use(express.static('public'))
 
+// SERVER CONNECTION
+app.listen(port, () => {
+  console.log(`Listening at PORT ${port}`)
+})
