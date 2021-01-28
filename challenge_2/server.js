cd3abc5936ec6ea379152ee2ef42acd1acb04e4d
Anthony Sim <anthonyysim@gmail.com>
2021-01-27 22:01:23 -0800

    refactored how methods were extracted

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const fs = require('fs')
-// const bodyParser = require('body-parser')
 
 // MIDDLEWARE FOR UPLOADS
 const multer = require('multer')
 
 // IMPORT OF METHODS CREATED TO MANIPULATE INCOMING DATA
-const mod = require('./modules/traverse')
-const csv = require('./modules/jsonToCSV')
+const { traverse } = require('./modules/traverse')
+const { convertCSV } = require('./modules/jsonToCSV')
 
 const app = express()
 const port = 3000
@@ -18,7 +17,7 @@ app.set('view engine', 'ejs');
 
 // STATIC FILE
 app.use(express.static(__dirname + '/client/views/stylesheets'));
-app.use(express.static('client'))
+
 
 // POST INCOMING DATA
 app.use(express.urlencoded({ extended: true }))
@@ -47,7 +46,7 @@ app.get('/download', (req, res) => {
 // let { data } = req.body;
 // parsedData = JSON.parse(data);
 
-// let traversedData = mod.traverse(parsedData)
+// let traversedData = traverse(parsedData)
 // console.log(traversedData)
 
 // res.render('./index', { data: traversedData })
@@ -71,8 +70,8 @@ app.post('/upload', upload.single('avatar'), (req, res) => {
   try {
     let data = fs.readFileSync(__dirname + '/uploads/sales_report.json')
     let parsedData = JSON.parse(data.toString());
-    let traversedData = mod.traverse(parsedData)
-    let csvData = csv.convertCSV(traversedData)
+    let traversedData = traverse(parsedData)
+    let csvData = convertCSV(traversedData)
 
     fs.writeFileSync(__dirname + '/uploads/csv_report.csv', csvData)
 
