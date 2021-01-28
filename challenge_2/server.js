7dae4b7f52b25093a52a3e2e0a90df2a470f5646
Anthony Sim <anthonyysim@gmail.com>
2021-01-27 20:27:47 -0800

    able to download

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -23,15 +23,36 @@ app.use(express.static('client'))
 // POST INCOMING DATA
 app.use(express.urlencoded({ extended: true }))
 // parse application/json
-// app.use(express.json())
-
-
+app.use(express.json())
 
 
 // GET ROUTE
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('./index')
 })
+
+// POST DOWNLOAD DATA
+app.get('/download', (req, res) => {
+  try {
+    let csvData = fs.readFileSync(__dirname + '/uploads/csv_report.csv')
+    res.send(csvData);
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+
+// POST UPLOAD TEXTAREA VERSION
+// app.post('/upload_json', (req, res) => {
+// TEXTAREA
+// let { data } = req.body;
+// parsedData = JSON.parse(data);
+
+// let traversedData = mod.traverse(parsedData)
+// console.log(traversedData)
+
+// res.render('./index', { data: traversedData })
+// })
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -45,38 +66,24 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
-// POST UPLOAD FILE PICKER / UPLOAD
-app.post('/upload_json', upload.single('avatar'), (req, res) => {
+// POST UPLOAD FILE PICKER / UPLOAD VERSION
+app.post('/upload', upload.single('avatar'), (req, res) => {
 
-  console.log(req)
-  res.render('./index')
+  try {
+    let data = fs.readFileSync(__dirname + '/uploads/sales_report.json')
+    let parsedData = JSON.parse(data.toString());
+    let traversedData = mod.traverse(parsedData)
+    let csvData = csv.convertCSV(traversedData)
+
+    fs.writeFileSync(__dirname + '/uploads/csv_report.csv', csvData)
+
+    res.render('./index', { data: traversedData })
+
+  } catch (err) {
+    console.error(err)
+  }
 })
 
-
-
-
-
-
-
-
-
-// POST UPLOAD JSON TEXTAREA
-// app.post('/upload_json', (req, res) => {
-// TEXTAREA
-// let { data } = req.body;
-// parsedData = JSON.parse(data);
-
-// let traversedData = mod.traverse(parsedData)
-// console.log(traversedData)
-
-// res.render('./index', { data: traversedData })
-// })
-
-// POST DOWNLOAD DATA
-// app.post('/download', (req, res) => {
-
-//   res.render('./index')
-// })
 
 
 // SERVER CONNECTION
