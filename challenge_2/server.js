23671be40bb0a5d02df5101c57953904f8f64488
Anthony Sim <anthonyysim@gmail.com>
2021-01-27 18:48:54 -0800

    file picker upload

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,39 +1,78 @@
 const express = require('express')
 const fs = require('fs')
+// const bodyParser = require('body-parser')
+
+// MIDDLEWARE FOR UPLOADS
+const multer = require('multer')
+
+// IMPORT OF METHODS CREATED TO MANIPULATE INCOMING DATA
 const mod = require('./modules/traverse')
 const csv = require('./modules/jsonToCSV')
 
 const app = express()
 const port = 3000
 
-// STATIC FILE
-// app.use(express.static('client'))
+// VIEW ENGINE
 app.set('views', './client/views')
 app.set('view engine', 'ejs');
 
+// STATIC FILE
+app.use(express.static(__dirname + '/client/views/stylesheets'));
+app.use(express.static('client'))
+
+// POST INCOMING DATA
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static(__dirname + '/client'));
+// parse application/json
+// app.use(express.json())
 
 
-// GETs
+
+
+// GET ROUTE
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.post('/upload_json', (req, res) => {
-
-
-  let { data } = req.body;
-  console.log(data)
-  // parsedData = JSON.parse(data);
-
-  // let traversedData = mod.traverse(parsedData)
-  // console.log(traversedData)
-
-  // res.render('./index', { data: traversedData })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/uploads')
+  },
+  filename: function (req, file, cb) {
+    const { originalname } = file;
+    cb(null, originalname)
+  }
 })
 
+const upload = multer({ storage: storage })
+
+// POST UPLOAD FILE PICKER / UPLOAD
+app.post('/upload_json', upload.single('avatar'), (req, res) => {
+
+  console.log(req)
+  res.render('./index')
+})
+
+
+
+
+
+
+
+
+
+// POST UPLOAD JSON TEXTAREA
+// app.post('/upload_json', (req, res) => {
+// TEXTAREA
+// let { data } = req.body;
+// parsedData = JSON.parse(data);
+
+// let traversedData = mod.traverse(parsedData)
+// console.log(traversedData)
+
+// res.render('./index', { data: traversedData })
+// })
+
+// POST DOWNLOAD DATA
 // app.post('/download', (req, res) => {
 
 //   res.render('./index')
