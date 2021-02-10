ccfa027c17bf646a81f883f806e2d4d2c2270af3
Anthony Sim <anthonyysim@gmail.com>
2021-02-09 17:56:14 -0800

    able to get started

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
 
 
+ReactDOM.render(<App />, document.getElementById('root'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+const express = require('express')
+const app = express()
 
 
+app.use(express.static('public'))
+
+
+// app.get('/', (req, res) => {
+
+// })
+
+const port = 3000
+app.listen(port, () => console.log(`Listening on port ${port}`))
