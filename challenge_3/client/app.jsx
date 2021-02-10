efa5dc1f16839c0aa9b7d5ba41969f2c02667452
Anthony Sim <anthonyysim@gmail.com>
2021-02-09 23:15:50 -0800

    general structure set up

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,58 @@
-const App = () => {
-  return (
-    <div>
-      Hello World!
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1,
+      name: '',
+      email: '',
+      password: '',
+      address1: '',
+      address2: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      phoneNumber: '',
+      creditCardNumber: '',
+      expiredDate: '',
+      CVV: '',
+      creditCardZipCode: ''
+    }
+    this.changeHandler = this.changeHandler.bind(this);
+    this.onbSubmit = this.onbSubmit.bind(this);
+  }
+
+  changeHandler(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  onbSubmit(e) {
+    e.preventDefault();
+    const { name, email, password, address1, address2, city, state, zipCode, phoneNumber, creditCardNumber, expiredDate, CVV, creditCardZipCode } = this.state;
+  }
+
+  render() {
+    console.log(this.state.step)
+    let formStep;
+
+    if (this.state.step === 1) {
+      formStep = <button>Checkout</button>
+    } else if (this.state.step === 2) {
+      formStep = <input type="text" value="name" />
+    }
+
+
+    return (
+      <div>
+        <h1>Checkout</h1>
+        <p>Step {this.state.step}</p>
+
+        {formStep}
+      </div >
+    )
+  }
 }
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -5,9 +5,5 @@ const app = express()
 app.use(express.static('public'))
 
 
-// app.get('/', (req, res) => {
-
-// })
-
-const port = 3000
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT} 👍!`))
