41b8e213a145d181303fbada86d0c8a28557db54
Anthony Sim <anthonyysim@gmail.com>
2021-02-13 13:01:54 -0800

    style and css loader webpack

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Board from './Board.jsx';
 
 
 const App = () => {
   return (
     <div>
-      Hello World!
+      <h1 className="display-4 text-center text-info">CONNECT FOUR</h1>
+      <Board />
     </div>
   )
 }
