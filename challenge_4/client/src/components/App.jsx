b1ad734f5aa3b18a38d5dcf51e9ce36a412fe723
Anthony Sim <anthonyysim@gmail.com>
2021-02-13 14:47:56 -0800

    grid table

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Board from './Board.jsx';
 
 
-const App = () => {
-  return (
-    <div>
-      <h1 className="display-4 text-center text-info">CONNECT FOUR</h1>
-      <Board />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="display-4 mt-3 text-center text-info">CONNECT FOUR</h1>
+        <Board />
+      </div>
+    )
+  }
 }
 
 export default App;
--- challenge_4/client/src/components/Board.jsx
+++ challenge_4/client/src/components/Board.jsx
@@ -1,8 +1,81 @@
 import React from 'react';
 
-const Board = () => {
+const Board = (props) => {
+
+
+  const styleOptions = {
+    minHeight: "60px"
+  }
   return (
-    <div></div>
+    < div class="container mt-5 border border-dark" >
+
+      {/* Row 1 */}
+      <div class="row text-center">
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+      </div>
+
+      {/* Row 2 */}
+      <div class="row text-center">
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+      </div>
+
+      {/* Row 3 */}
+      <div class="row text-center">
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+      </div>
+
+      {/* Row 4 */}
+      <div class="row text-center">
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+      </div>
+
+      {/* Row 5 */}
+      <div class="row text-center">
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+      </div>
+
+      {/* Row 6 */}
+      <div class="row text-center">
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+        <div class="col-sm my-auto border-right border-bottom">$</div>
+      </div>
+
+    </div >
   )
 }
 
