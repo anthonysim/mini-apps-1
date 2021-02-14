8a836d2e1b9263a8008d86df8be575f63b9c3291
Anthony Sim <anthonyysim@gmail.com>
2021-02-13 19:14:11 -0800

    filled board

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -6,7 +6,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      board: [
+        ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
+      ]
     }
   }
 
@@ -15,7 +22,7 @@ class App extends Component {
       <div>
         <h1 className="display-4 mt-3 text-center text-info">CONNECT FOUR</h1>
         <br />
-        <Board />
+        <Board pieces={this.state.board} />
       </div>
     )
   }
--- challenge_4/client/src/components/Board.jsx
+++ challenge_4/client/src/components/Board.jsx
@@ -1,80 +1,72 @@
 import React from 'react';
 
 
-const Board = (props) => {
+const Board = ({ pieces }) => {
+  let result = '';
 
-
-  const styleOptions = {
-    minHeight: "60px"
-  }
   return (
-    < div style={{ width: "60%" }} class="container mt-5 border border-dark" >
+    < div style={{ width: "60%" }
+    } className="container mt-5 border border-dark" >
 
       {/* Row 1 */}
-      <div class="row text-center">
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-      </div>
+      <div className="row text-center" >
+        <div className="col-sm my-auto border-right border-bottom">{pieces[0][0]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[0][1]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[0][2]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[0][3]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[0][4]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[0][5]}</div>
+      </div >
 
       {/* Row 2 */}
-      <div class="row text-center">
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-      </div>
+      <div className="row text-center" >
+        <div className="col-sm my-auto border-right border-bottom">{pieces[1][0]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[1][1]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[1][2]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[1][3]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[1][4]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[1][5]}</div>
+      </div >
 
       {/* Row 3 */}
-      <div class="row text-center">
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-      </div>
+      <div className="row text-center" >
+        <div className="col-sm my-auto border-right border-bottom">{pieces[2][0]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[2][1]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[2][2]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[2][3]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[2][4]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[2][5]}</div>
+      </div >
 
       {/* Row 4 */}
-      <div class="row text-center">
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-      </div>
+      <div className="row text-center" >
+        <div className="col-sm my-auto border-right border-bottom">{pieces[3][0]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[3][1]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[3][2]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[3][3]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[3][4]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[3][5]}</div>
+      </div >
 
       {/* Row 5 */}
-      <div class="row text-center">
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-      </div>
+      <div className="row text-center" >
+        <div className="col-sm my-auto border-right border-bottom">{pieces[4][0]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[4][1]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[4][2]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[4][3]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[4][4]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[4][5]}</div>
+      </div >
 
       {/* Row 6 */}
-      <div class="row text-center">
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-        <div class="col-sm my-auto border-right border-bottom">$</div>
-      </div>
+      <div className="row text-center" >
+        <div className="col-sm my-auto border-right border-bottom">{pieces[5][0]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[5][1]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[5][2]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[5][3]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[5][4]}</div>
+        <div className="col-sm my-auto border-right border-bottom">{pieces[5][5]}</div>
+      </div >
 
     </div >
   )
